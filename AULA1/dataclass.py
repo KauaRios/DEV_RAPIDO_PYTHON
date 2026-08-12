dc8f96fd7d61532547a64e8d95da90efbc0f2029
KauaRios <kauarios49@gmail.com>
2026-08-12 11:44:07 -0300

    fix bug

--- AULA1/dataclass.py
+++ AULA1/dataclass.py
@@ -8,9 +8,9 @@ class Aluno:
         return sum(self.notas) / len(self.notas)
 \
 
-    if __name__ == "__main__":
-        aluno1=Aluno("Kauã",20,[8,7,9,8])
-        aluno2=Aluno("pedro",15,[6,7,3,8])
-        lista_alunos=(aluno1,aluno2)
-        for aluno in lista_alunos:
-            print(f"Nome:{aluno.nome},média: {aluno.calcular_media():2.f}")
+if __name__ == "__main__":
+    aluno1=Aluno("Kauã",20,[8,7,9,8])
+    aluno2=Aluno("pedro",15,[6,7,3,8])
+    lista_alunos=(aluno1,aluno2)
+    for aluno in lista_alunos:
+        print(f"Nome:{aluno.nome},média: {aluno.calcular():.2f}")
