752ceadad7ccdf9390e3ab01a5a157f8d71737fa
KauaRios <kauarios49@gmail.com>
2026-08-31 17:37:04 -0300

    Adicionando Exercicios

---
+++ ESTUDOS_CASA/import_classe.py
@@ -0,0 +1,13 @@
+from classes import Vendedor
+
+
+vendedor1=Vendedor("Kaua")
+vendedor1.vendeu(1000)
+vendedor1.bateu_meta(600)
+print(vendedor1.nome)
+
+print(vendedor1.vendas)
+
+vendedor2=Vendedor("Alon")
+vendedor2.vendeu(300)
+vendedor2.bateu_meta(600)
